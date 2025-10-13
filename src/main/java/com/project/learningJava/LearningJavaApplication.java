b04e50a48096e6374b1c3db2a2242f55a03b6fb3
Babs20200 <82280307+Babs20200@users.noreply.github.com>
2025-10-13 16:09:29 +0100

    first commit

---
+++ src/main/java/com/project/learningJava/LearningJavaApplication.java
@@ -0,0 +1,13 @@
+package com.project.learningJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningJavaApplication.class, args);
+	}
+
+}
