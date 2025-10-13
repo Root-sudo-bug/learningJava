e4fbe2680b6ee8a45b2a2d3ee3edc6334dba4a2c
Babs20200 <82280307+Babs20200@users.noreply.github.com>
2025-10-13 18:40:56 +0100

    Syntax

--- src/main/java/com/project/learningJava/PrintNumber.java
+++ src/main/java/com/project/learningJava/PrintNumber.java
@@ -1,4 +1,13 @@
 package com.project.learningJava;
 
 public class PrintNumber {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(3+3);
+        System.out.println(4+4);
+        System.out.println(2*5);
+        System.out.println(10/2);
+
+
+    }
 }
